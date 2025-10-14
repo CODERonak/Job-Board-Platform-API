fc155b9af4cf7609ff8c3f7ae2cf186094a42432
r9988.2588 <r9988.2588@gmail.com>
2025-10-14 04:01:30 +0000

    create: create users mapper

---
+++ src/main/java/com/project/JobBoardAPI/mapper/UsersMapper.java
@@ -0,0 +1,25 @@
+package com.project.JobBoardAPI.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.project.JobBoardAPI.dto.users.*;
+import com.project.JobBoardAPI.model.entity.Users;
+
+@Mapper(componentModel = "spring")
+public interface UsersMapper {
+
+    // Maps RegisterRequest to Users entity
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    Users toEntity(RegisterRequest request);
+
+    // Maps Users entity to RegisterResponse
+    @Mapping(target = "token", ignore = true)
+    RegisterResponse toRegisterResponse(Users entity);
+
+    // Maps Users entity to LoginResponse
+    @Mapping(target = "token", ignore = true)
+    RegisterResponse toLoginResponse(Users entity);
+}
