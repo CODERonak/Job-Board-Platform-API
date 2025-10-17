31c2b3e7f5771818017ac484c0fd8256b7e19522
r9988.2588 <r9988.2588@gmail.com>
2025-10-17 05:11:03 +0000

    create: create AccessDeniedException

--- src/main/java/com/project/JobBoardAPI/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/project/JobBoardAPI/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.project.JobBoardAPI.exceptions;
 
+import java.nio.file.AccessDeniedException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,36 +11,52 @@ import com.project.JobBoardAPI.exceptions.custom.AlreadyExistsException;
 import com.project.JobBoardAPI.exceptions.custom.InvalidCredentials;
 import com.project.JobBoardAPI.exceptions.custom.NotFoundException;
 
+// Global exception handler for the application
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleNotFoundException(
-            NotFoundException ex) {
-        ErrorResponse error = new ErrorResponse(
-                HttpStatus.NOT_FOUND.value(),
-                HttpStatus.NOT_FOUND.getReasonPhrase(),
-                ex.getMessage());
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
 
-    @ExceptionHandler(AlreadyExistsException.class)
-    public ResponseEntity<ErrorResponse> handleAlreadyExistsException(
-            AlreadyExistsException ex) {
-        ErrorResponse error = new ErrorResponse(
-                HttpStatus.CONFLICT.value(),
-                HttpStatus.CONFLICT.getReasonPhrase(),
-                ex.getMessage());
-        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
-    }
+        // NotFound Exception for handling not found errors
+        @ExceptionHandler(NotFoundException.class)
+        public ResponseEntity<ErrorResponse> handleNotFoundException(
+                        NotFoundException ex) {
+                ErrorResponse error = new ErrorResponse(
+                                HttpStatus.NOT_FOUND.value(),
+                                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                                ex.getMessage());
+                return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        }
 
-    @ExceptionHandler(InvalidCredentials.class)
-    public ResponseEntity<ErrorResponse> handleInvalidCredentials(
-            InvalidCredentials ex) {
-        ErrorResponse error = new ErrorResponse(
-                HttpStatus.UNAUTHORIZED.value(),
-                HttpStatus.UNAUTHORIZED.getReasonPhrase(),
-                ex.getMessage());
-        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
-    }
+        // AlreadyExists Exception for handling already exists errors
+        @ExceptionHandler(AlreadyExistsException.class)
+        public ResponseEntity<ErrorResponse> handleAlreadyExistsException(
+                        AlreadyExistsException ex) {
+                ErrorResponse error = new ErrorResponse(
+                                HttpStatus.CONFLICT.value(),
+                                HttpStatus.CONFLICT.getReasonPhrase(),
+                                ex.getMessage());
+                return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+        }
+
+        // InvalidCredentials Exception for handling invalid credentials errors
+        @ExceptionHandler(InvalidCredentials.class)
+        public ResponseEntity<ErrorResponse> handleInvalidCredentials(
+                        InvalidCredentials ex) {
+                ErrorResponse error = new ErrorResponse(
+                                HttpStatus.UNAUTHORIZED.value(),
+                                HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                                ex.getMessage());
+                return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+        }
+
+        // AccessDenied Exception for handling access denied errors
+        @ExceptionHandler(AccessDeniedException.class)
+        public ResponseEntity<ErrorResponse> handleAccessDeniedException(
+                        AccessDeniedException ex) {
+                ErrorResponse error = new ErrorResponse(
+                                HttpStatus.FORBIDDEN.value(),
+                                HttpStatus.FORBIDDEN.getReasonPhrase(),
+                                ex.getMessage());
+                return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
+        }
 
 }
